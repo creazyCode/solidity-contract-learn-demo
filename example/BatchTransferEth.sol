2d59d914029d984f915c44cff4b133ef538113e3
achilles <xavier19921229@gmail.com>
2024-03-25 23:54:12 +0800

    Update BatchTransferEth.sol

--- example/BatchTransferEth.sol
+++ example/BatchTransferEth.sol
@@ -14,8 +14,12 @@ contract BatchTransferEth {
           owner = payable(msg.sender);
      }
 
-     //批量转ETH给所有地址，数量平均分配，调用时指定的 value 值即总的 ETH 数量
-     function batchTransferETHToAll(address[] calldata _toAddresses ) payable ownable public returns (bool){
+     function getBalance() public returns(uint256){
+          return address(this).balance;
+     }
+
+     //批量转ETH给所有地址，ETH数量平均分配，调用时指定的 value 值即总的 ETH 数量
+     function transferAvgEthToAll(address[] calldata _toAddresses ) payable ownable public returns (bool){
           require(_toAddresses.length > 0, "toAddresses is empty");
           uint avgEth = address(this).balance / _toAddresses.length; 
           for(uint32 i = 0; i < _toAddresses.length; i++){
@@ -23,4 +27,16 @@ contract BatchTransferEth {
           }
           return true;
      }
+
+     //批量转ETH给所有地址，ETH 指定分配
+     function transferDiffEthToAll(address[] calldata _toAddresses, uint256[] calldata values) payable ownable public returns (bool){
+          require(_toAddresses.length > 0, "toAddresses is empty");
+          require(_toAddresses.length <= values.length, "values invalid");
+          for(uint32 i = 0; i < _toAddresses.length; i++){
+               payable(_toAddresses[i]).transfer(values[i]);
+          }
+          return true;
+     }
+
+     
 }
